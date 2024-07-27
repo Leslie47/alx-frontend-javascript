321ec9a4f40e5f52a65bb96453475007889e44c3
Leslie Asante <leslieasante44@gmail.com>
2024-07-27 00:46:23 +0000

    es6 promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
+
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
+}
